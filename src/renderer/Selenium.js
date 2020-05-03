df33a11d6c8ca5917bc250f3ed3bb215743f4704
Vladimir Pakhomchik <v.pahomchik@gmail.com>
2020-05-03 08:58:52 +0800

    Init commit.

---
+++ src/renderer/Selenium.js
@@ -0,0 +1,16 @@
+export default class Selenium {
+    constructor() {
+
+    }
+
+    run() {
+        const webdriver = require('selenium-webdriver')
+        const browser = new webdriver.Builder().usingServer().withCapabilities({'browserName': 'chrome' }).build()
+
+        browser.get('http://en.wikipedia.org/wiki/Wiki')
+        browser.findElements(webdriver.By.css('[href^="/wiki/"]')).then(function(links) {
+            console.log('Found', links.length, 'Wiki links.')
+            browser.quit()
+        });
+    }
+}
